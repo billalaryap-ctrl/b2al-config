b0738f5d895b72546bf7be93c485df3b3bd7389d
billalaryap-ctrl <billalaryap@gmail.com>
2026-01-05 16:21:04 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,13 +8,9 @@ return {
       pass = "0",
       key  = "B2AL-vip",
 
-      -- kosong = siap daftar HWID
       hwid = "",
-
       expire = "2026-01-01",
 
-      -- ban = true → blokir login
-      -- ban = false → aktif
       ban = false,
     }
 
