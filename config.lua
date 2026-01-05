1849bec4010be50983ba2e324716ea9e7aa4018e
billalaryap-ctrl <billalaryap@gmail.com>
2026-01-05 15:07:54 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+{
+  global_expire = "2026-01-07",
+
+  users = {
+
+    {
+      user = "b2al",
+      pass = "0",
+      key  = "B2AL-6969",
+      hwid = nil,
+      expire = "2026-01-07"
+    }
+
+  }
+}
